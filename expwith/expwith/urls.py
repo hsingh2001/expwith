65d1d919b4cf7b7aefaffda63ec35671023e7f41
Harvinder Singh <hsingh2000@gmail.com>
2014-01-29 08:36:53 -0800

    Add homepage app

--- expwith/expwith/urls.py
+++ expwith/expwith/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
 import register
+import homepage
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'expwith.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^register/', include('register.urls')),
+    url(r'^$', include('homepage.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^register/', include('register.urls')),
 )
